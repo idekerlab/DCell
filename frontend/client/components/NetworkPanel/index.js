92382473cc021e6c2f30b7671c5475a611938b89
Keiichiro Ono <keiono@gmail.com>
2017-01-03 19:09:22 -0800

    CLIXO test added.

--- frontend/client/components/NetworkPanel/index.js
+++ frontend/client/components/NetworkPanel/index.js
@@ -14,9 +14,11 @@ import FlatButton from 'material-ui/FlatButton'
 
 import style from './style.css'
 
-const cyjsUrl = 'https://gist.githubusercontent.com/keiono/b7c047c1166681ef7170881217819938/raw/b81de1da63075ceb0e4c8d99adeacf83d0193cdb/goWOgenes.cyjs'
+// const cyjsUrl = 'https://gist.githubusercontent.com/keiono/b7c047c1166681ef7170881217819938/raw/b81de1da63075ceb0e4c8d99adeacf83d0193cdb/goWOgenes.cyjs'
 // const cyjsUrl = 'https://raw.githubusercontent.com/idekerlab/ontology-data-generators/master/atgo.cyjs'
 // const cyjsUrl = 'https://gist.githubusercontent.com/keiono/004744a332451a472bf85c8beefba9db/raw/0955dddc4a3ea5a0e87423a349e5b2b16ec46fa2/gotree2.cyjs'
+
+const cyjsUrl = 'https://gist.githubusercontent.com/keiono/2b2e289371b7aff1f47d5e2c2a41fc2a/raw/37bb6d2bd1d81072f95721fd83d88ac3774e365c/clixo-final.cyjs'
 
 
 class NetworkPanel extends Component {
